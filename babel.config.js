8cabb35bb5800eeb07eaf9e2cc2e790d08dbfb24
ilgoncalves <lima-igor@hotmail.com>
2023-07-22 00:38:49 -0300

    chore: add configs, install libs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,32 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+const rootImportOpts = {
+  root: __dirname,
+  rootPathSuffix: './src',
+  rootPathPrefix: '~/',
+};
+
+module.exports = api => {
+  api.cache(true);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      ['babel-plugin-root-import', rootImportOpts],
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+      [
+        'react-native-reanimated/plugin',
+        {
+          globals: ['__scanCodes'],
+        },
+      ],
+    ],
+  };
 };
