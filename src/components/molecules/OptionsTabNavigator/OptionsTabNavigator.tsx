56f64aa654e1243e94b299d495aeefc0343ab132
ilgoncalves <lima-igor@hotmail.com>
2023-07-22 19:47:50 -0300

    feat: create OptionsTabNavigator

---
+++ src/components/molecules/OptionsTabNavigator/OptionsTabNavigator.tsx
@@ -0,0 +1,56 @@
+import { FC } from 'react';
+import { Div, Text } from 'react-native-magnus';
+import { OptionsTabNavigatorProps } from './types';
+import { ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import { getFontWeight } from '~/theme/components/Text';
+import LinearGradient from 'react-native-linear-gradient';
+import { colors } from '~/theme/colors';
+
+const OptionsTabNavigator: FC<OptionsTabNavigatorProps> = ({
+  tabs,
+  activeTabKey,
+  onTabPress,
+}) => {
+  return (
+    <Div>
+      <LinearGradient
+        colors={[colors.transparent, colors['background.linearBackgroundDark']]}
+        style={styles.gradient}
+      />
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <Div row justifyContent="space-between">
+          {tabs.map(tab => (
+            <TouchableOpacity onPress={() => onTabPress(tab.key)}>
+              <Text
+                px="xl"
+                py="md"
+                key={tab.key}
+                variant="medium"
+                {...getFontWeight('bold')}
+                color={
+                  activeTabKey === tab.key
+                    ? 'primary.pink'
+                    : 'secondary.textOpaque'
+                }
+                onPress={() => onTabPress(tab.key)}>
+                {tab.value}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </Div>
+      </ScrollView>
+    </Div>
+  );
+};
+
+const styles = StyleSheet.create({
+  gradient: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '100%',
+  },
+});
+
+export { OptionsTabNavigator };
