cc2e6434578e87507656d1d0e1e5f34293fc180f
ilgoncalves <lima-igor@hotmail.com>
2023-07-22 15:36:05 -0300

    feat: add MainTemplate to Home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,9 +1,20 @@
-import React, { FC } from 'react';
-import { Div } from 'react-native-magnus';
+import { FC } from 'react';
 import { HomeProps } from './types';
+import { MainTemplate } from '~/components/templates';
+import { TranslationsKeys } from '~/i18n';
+import { Text } from 'react-native-magnus';
+import { useTranslation } from 'react-i18next';
 
 const Home: FC<HomeProps> = ({}) => {
-  return <Div></Div>;
+  const { t } = useTranslation();
+
+  return (
+    <MainTemplate
+      centralizeTitle={false}
+      title={t(TranslationsKeys.HomeWelcome, { name: 'Igor' })}>
+      <Text>Home</Text>
+    </MainTemplate>
+  );
 };
 
 export { Home };
