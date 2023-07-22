38526fc2e1d69b15a1599b2cda7f9e68a7ac8599
ilgoncalves <lima-igor@hotmail.com>
2023-07-22 15:36:05 -0300

    feat: add MainTemplate to Home

--- src/i18n/locales/pt-br.ts
+++ src/i18n/locales/pt-br.ts
@@ -2,6 +2,7 @@ import TranslationKeyEnum, { TranslationKey } from '../translation.keys';
 
 const translations: TranslationKey = {
   [TranslationKeyEnum.LoginWelcome]: 'Welcome',
+  [TranslationKeyEnum.HomeWelcome]: 'Bem-vindo, {{name}}!',
 };
 
 export default { translation: translations };
--- src/i18n/translation.keys.ts
+++ src/i18n/translation.keys.ts
@@ -1,6 +1,7 @@
 // Pattern is ${PageName}${Description} = '${pagename}-description'
 export enum TranslationKeyEnum {
   LoginWelcome = 'login-welcome',
+  HomeWelcome = 'home-welcome',
 }
 
 export type TranslationKey = { [K in TranslationKeyEnum]: string };
