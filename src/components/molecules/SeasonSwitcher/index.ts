75880bc08c8a359d3962096f622bdf55aeddde95
ilgoncalves <lima-igor@hotmail.com>
2023-07-22 23:05:17 -0300

    feat: create seasonSwitcher

---
+++ src/components/molecules/SeasonSwitcher/index.ts
@@ -0,0 +1,2 @@
+export * from './SeasonSwitcher';
+export * from './types';
---
+++ src/components/molecules/SeasonSwitcher/types.ts
@@ -0,0 +1,4 @@
+export interface SeasonSwitcherProps {
+  totalSeasons: number;
+  onSeasonChange: (season: number) => void;
+}
